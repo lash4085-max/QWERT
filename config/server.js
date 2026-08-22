c5ea0a849724998a32062aca17af7e28d4d23f4c
Abhi <lash4085@gmail.com>
2026-08-22 15:29:08 +0530

    Update API route for attendance

--- config/server.js
+++ config/server.js
@@ -16,7 +16,7 @@ app.get("/", (req, res) => {
   res.send("Dayflow HRMS API is running");
 });
 
-app.use("/api/auth", authRoutes);
+app.use("/api/attendance", require("./routes/attendance"));
 
 // TODO (team - split these up):
 // app.use("/api/employees", require("./routes/employees"));   -> profile view/edit
